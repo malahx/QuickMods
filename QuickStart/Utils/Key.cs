5484921ff3f0f8f403bb27eab1af1fc2bd50241b
Malah <malah@neuf.fr>
2017-05-26 23:07:14 +0200

    added a forgotten file

---
+++ QuickStart/Utils/Key.cs
@@ -0,0 +1,156 @@
+﻿/* 
+QuickStart
+Copyright 2017 Malah
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+using UnityEngine;
+
+namespace QuickStart.QUtils {
+    static class QKey {
+
+        internal static Key setKey = Key.None;
+
+        static Rect rectSetKey = new Rect();
+        static Rect RectSetKey {
+            get {
+                if (rectSetKey.position == Vector2.zero && rectSetKey.size != Vector2.zero) {
+                    rectSetKey.x = (Screen.width - rectSetKey.width) / 2;
+                    rectSetKey.y = (Screen.height - rectSetKey.height) / 2;
+                }
+                return rectSetKey;
+            }
+            set {
+                rectSetKey = value;
+            }
+        }
+
+
+        internal enum Key {
+            None,
+            Escape
+        }
+
+        static string GetText(Key key) {
+            switch (key) {
+                case Key.Escape:
+                    return QLang.translate("Stop QuickStart");
+            }
+            return string.Empty;
+        }
+
+        static KeyCode CurrentKey(Key key) {
+            switch (key) {
+                case Key.Escape:
+                    return QSettings.Instance.KeyEscape;
+            }
+            return KeyCode.None;
+        }
+
+        static void SetCurrentKey(Key key, KeyCode currentKey) {
+            switch (key) {
+                case Key.Escape:
+                    QSettings.Instance.KeyEscape = currentKey;
+                    break;
+            }
+            QDebug.Log(string.Format("SetCurrentKey({0}): {1}", GetText(key), currentKey), "QKey");
+        }
+
+        static void VerifyKey(Key key) {
+            try {
+                Input.GetKey(CurrentKey(key));
+            } catch {
+                QDebug.Warning("Wrong key: " + CurrentKey(key), "QKey");
+                SetCurrentKey(key, DefaultKey(key));
+            }
+        }
+
+        internal static void VerifyKey() {
+            VerifyKey(Key.Escape);
+            QDebug.Log("VerifyKey", "QKey");
+        }
+
+        internal static KeyCode DefaultKey(Key key) {
+            switch (key) {
+                case Key.Escape:
+                    return KeyCode.Escape;
+            }
+            return KeyCode.None;
+        }
+
+        internal static bool isKeyDown(Key key) {
+            return Input.GetKeyDown(CurrentKey(key));
+        }
+
+        internal static bool SetKey() {
+            if (setKey == Key.None) {
+                return false;
+            }
+            if (Event.current.isKey) {
+                KeyCode key = Event.current.keyCode;
+                if (key != KeyCode.None) {
+                    SetCurrentKey(setKey, key);
+                    setKey = Key.None;
+                }
+            }
+            return true;
+        }
+
+        internal static void DrawSetKey() {
+            if (setKey == Key.None) {
+                return;
+            }
+            RectSetKey = GUILayout.Window(1545156, RectSetKey, DrawSetKey, string.Format("{0} {1}", QLang.translate("Set Key:"), GetText(setKey)), GUILayout.ExpandHeight(true));
+        }
+
+        static void DrawSetKey(int id) {
+            GUILayout.BeginVertical();
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(string.Format("{0} <color=#FFFFFF><b>{1}</b></color>", QLang.translate("Press a key to select the"), GetText(setKey)));
+            GUILayout.EndHorizontal();
+            GUILayout.Space(5);
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button(QLang.translate("Clear Assignment"), GUILayout.ExpandWidth(true), GUILayout.Height(30))) {
+                SetCurrentKey(setKey, KeyCode.None);
+                setKey = Key.None;
+            }
+            if (GUILayout.Button(QLang.translate("Default Assignment"), GUILayout.ExpandWidth(true), GUILayout.Height(30))) {
+                SetCurrentKey(setKey, DefaultKey(setKey));
+                setKey = Key.None;
+            }
+            if (GUILayout.Button(QLang.translate("Cancel Assignment"), GUILayout.ExpandWidth(true), GUILayout.Height(30))) {
+                setKey = Key.None;
+            }
+            GUILayout.EndHorizontal();
+            GUILayout.Space(5);
+            GUILayout.EndVertical();
+        }
+
+        internal static void DrawConfigKey() {
+            DrawConfigKey(Key.Escape);
+        }
+
+        static void DrawConfigKey(Key key) {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(string.Format("{0}: <color=#FFFFFF><b>{1}</b></color>", GetText(key), CurrentKey(key)), GUILayout.Width(350));
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button(QLang.translate("Set"), GUILayout.ExpandWidth(true), GUILayout.Height(20))) {
+                setKey = key;
+            }
+            GUILayout.EndHorizontal();
+            GUILayout.Space(5);
+        }
+    }
+}
