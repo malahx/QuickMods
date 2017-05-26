c8c946afd1b1bef5551cbf474310b2c7aa0d0fe6
Malah <malah@neuf.fr>
2017-05-26 20:08:33 +0200

    deleted deprecated quickmods

--- QuickEngineer/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-VAR=QuickEngineer
-cp -f bin/${VAR}.dll GameData/${VAR}/Plugins/
-cp -f VesselSimulator/bin/VesselSimulator-QE.dll GameData/${VAR}/Plugins/
-cp -f README.md GameData/${VAR}/
-cp -f COPYING GameData/${VAR}/
-cp -f ${VAR}.version GameData/${VAR}/
-rm -rf ../00KSP-dev/GameData/${VAR}/
-cp -rf GameData/${VAR} ../00KSP-dev/GameData/${VAR}
