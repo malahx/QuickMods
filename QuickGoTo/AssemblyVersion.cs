9bfe39bda2636b9daff1f6d8cc160b2bd58d928d
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-07-07 10:29:35 -0400

    Updated all AssemblyVersion.tt files  Rebuild for KSP 1.7.2

--- QuickGoTo/AssemblyVersion.cs
+++ QuickGoTo/AssemblyVersion.cs
@@ -5,4 +5,4 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.3.5.2")]
+ [assembly: AssemblyVersion("1.3.5.1")]
--- QuickIVA/AssemblyVersion.cs
+++ QuickIVA/AssemblyVersion.cs
@@ -5,4 +5,4 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.2.4.2")]
+ [assembly: AssemblyVersion("1.2.4.1")]
