b45db031ebd165acdaf94a41596f71f0c9d9ed60
malahx <malahx@users.noreply.github.com>
2021-09-13 00:35:59 +0200

    [QuickManeuver] initial commits

---
+++ QuickManeuver/QO.cs
@@ -0,0 +1,46 @@
+﻿/* 
+QuickManeuver
+Copyright 2017 Malah
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+using System;
+using UnityEngine;
+
+namespace QuickManeuver {
+
+	[KSPAddon(KSPAddon.Startup.Flight, false)]
+	public class QuickManeuver : MonoBehaviour {
+		private void Awake() {
+			GameEvents.OnFlightGlobalsReady.Add (OnFlightGlobalsReady);
+			Debug.Log ("[QuickManeuver] Awake");
+		}
+
+		private void OnFlightGlobalsReady(bool ready) {
+			if (!ready || FlightGlobals.ActiveVessel == null) {
+				return;
+			}
+			FlightUIModeController.Instance.SetMode(FlightUIMode.MANEUVER_INFO);
+			FlightUIModeController.Instance.maneuverButton.ButtonCtrl.onClick.Invoke();
+			Debug.LogFormat("[QuickManeuver] Activate maneuver info");
+			Destroy(this);
+		}
+
+		private void OnDestroy() {
+			GameEvents.OnFlightGlobalsReady.Remove (OnFlightGlobalsReady);
+			Debug.Log ("[QuickManeuver] OnDestroy");
+		}
+	}
+}
---
+++ QuickManeuver/README.md
@@ -0,0 +1,47 @@
+﻿# QuickOrbit
+#### A plugin for Kerbal Space Program
+#### Copyright 2021 Malah
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+
+
+#### What is it?
+
+QuickOrbit is a plugin which adds the possibility to activate orbit information at the load of a vessel.
+
+#### How to install it?
+
+Unzip all files. Put the QuickMods folder in your KSP/GameData folder.
+
+#### How to update it?
+
+Unzip all files. Merge the new QuickMods folder with the old folder which is in your KSP/GameData folder.
+
+#### How to uninstall it?
+
+Delete the QuickMods/QuickOrbit folder in your KSP/GameData folder.
+
+#### Changelog
+v1.0.0 - 2021.09.12
+* First release
+
+#### Thanks!
+
+* to all other mod developers which make this game really huge,
+* and to Squad for this awesome game.
+
+#### Links
+
+* http://forum.kerbalspaceprogram.com/index.php?/topic/85834-1
+* https://github.com/malahx/QuickMods
