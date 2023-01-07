139ea3e69917c789bafd9561178b4f54ff409efe
Jonathan Bayer <linuxgurugamer@gmail.com>
2023-01-07 14:41:41 -0500

    QuickExit 2.2.0.9  Version bump to fix embedded version file
     Overall
      Fixed local build script to properly copy version file

--- QuickExit/AssemblyVersion.cs
+++ QuickExit/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("2.2.0.8")]
- [assembly: AssemblyFileVersion("2.2.0.8")]
+ [assembly: AssemblyVersion("2.2.0.9")]
+ [assembly: AssemblyFileVersion("2.2.0.9")]
