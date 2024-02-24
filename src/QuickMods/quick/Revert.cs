ad751441c90b4595a57ade9e461f221d2a324c7c
malahx <malahx@users.noreply.github.com>
2024-02-24 18:42:42 +0100

    added notifications

--- src/QuickMods/quick/Revert.cs
+++ src/QuickMods/quick/Revert.cs
@@ -1,3 +1,4 @@
+using KSP.Game;
 using KSP.Messages;
 using QuickMods.configuration;
 using static KSP.Sim.impl.VesselSituations;
@@ -29,6 +30,15 @@ public class Revert(RevertConfiguration config) : ModsBase(config)
         Game.stateRevTracker._saveGameData = null;
         Game.stateRevTracker._lastLaunchedAssemblyName = null;
         Game.stateRevTracker._lastVesselCreatedName = null;
+        
+        var notificationData = new NotificationData
+        {
+            Tier = NotificationTier.Passive,
+            Primary = new NotificationLineItemData { LocKey = "QuickMods/QuickRevert/Notifications/LostRevert/Primary" },
+            FirstLine = new NotificationLineItemData { LocKey = "QuickMods/QuickRevert/Notifications/LostRevert/FirstLine" },
+            Importance = NotificationImportance.Low
+        };
+        Game.Notifications.ProcessNotification(notificationData);
 
         Logger.LogDebug("Lost.");
     }
--- src/QuickMods/quick/StopWarp.cs
+++ src/QuickMods/quick/StopWarp.cs
@@ -1,3 +1,4 @@
+using KSP.Game;
 using KSP.Messages;
 using QuickMods.configuration;
 
@@ -19,9 +20,19 @@ public class StopWarp(StopWarpConfiguration config) : ModsBase(config)
 
     private void OnVesselSituationChange(MessageCenterMessage msg)
     {
-        if (!config.VesselSituationChange() || msg is not VesselSituationChangedMessage message) return;
+        if (!config.VesselSituationChange() || msg is not VesselSituationChangedMessage message || !Game.ViewController.TimeWarp.IsWarping) return;
 
         Game.ViewController.TimeWarp.StopTimeWarp(true);
+
+        var notificationData = new NotificationData
+        {
+            Tier = NotificationTier.Passive,
+            Primary = new NotificationLineItemData { LocKey = "QuickMods/StopWarp/Notifications/VesselSituationChange/Primary" },
+            FirstLine = new NotificationLineItemData { LocKey = "QuickMods/StopWarp/Notifications/VesselSituationChange/FirstLine", ObjectParams = [message.OldSituation, message.NewSituation] },
+            Importance = NotificationImportance.Low
+        };
+        Game.Notifications.ProcessNotification(notificationData);
+
         Logger.LogDebug($"Stop wrap, VesselName: {message.Vessel.Name}, isActiveVessel: {Game.ViewController.IsActiveVessel(message.Vessel)}, Old Situation: {message.OldSituation}, New Situation; {message.NewSituation}");
     }
 }
