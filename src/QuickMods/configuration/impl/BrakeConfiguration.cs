f56b53844367e60b60347feda8886914a1c2f230
malahx <malahx@users.noreply.github.com>
2024-03-09 16:23:25 +0100

    enhanced brake key, keep stock key as it was

--- src/QuickMods/configuration/impl/BrakeConfiguration.cs
+++ src/QuickMods/configuration/impl/BrakeConfiguration.cs
@@ -1,10 +1,11 @@
 using BepInEx.Configuration;
+using UnityEngine;
 
 namespace QuickMods.configuration.impl;
 
 public class BrakeConfiguration(ConfigFile config) : ConfigurationBase("QuickBrake")
 {
-    private ConfigEntry<ToggleBrakeEnum> _toggleBrake;
+    private ConfigEntry<KeyboardShortcut> _toggleBrakeKey;
     private ConfigEntry<bool> _unBrakeAtLaunch;
     private ConfigEntry<bool> _brakeWhenControlLost;
     private ConfigEntry<bool> _brakePreLaunchAtLoad;
@@ -12,7 +13,7 @@ public class BrakeConfiguration(ConfigFile config) : ConfigurationBase("QuickBra
     private ConfigEntry<bool> _brakeLandedPlaneAtLoad;
     private ConfigEntry<bool> _brakeLandedVesselAtLoad;
 
-    public ToggleBrakeEnum ToggleBrake() => _toggleBrake.Value;
+    public KeyboardShortcut ToggleBrakeKey() => _toggleBrakeKey.Value;
     public bool UnBrakeAtLaunch() => _unBrakeAtLaunch.Value;
     public bool BrakeWhenControlLost() => _brakeWhenControlLost.Value;
     public bool BrakePreLaunchAtLoad() => _brakePreLaunchAtLoad.Value;
@@ -24,19 +25,12 @@ public class BrakeConfiguration(ConfigFile config) : ConfigurationBase("QuickBra
     {
         base.Init();
 
-        _toggleBrake = config.Bind("QuickMods/Brake", "ToggleBrake", ToggleBrakeEnum.StockBrake, "StockBrake is no toggle feature like in Stock KSP, Modifier enable toggle only when press modifier+brake key");
+        _toggleBrakeKey = config.Bind("QuickMods/Brake", "ToggleBrakeKey", new KeyboardShortcut(KeyCode.B, KeyCode.LeftAlt), "The key to toggle brake");
         _unBrakeAtLaunch = config.Bind("QuickMods/Brake", "UnBrakeAtLaunch", false, "Enable or disable un brake after launch");
         _brakeWhenControlLost = config.Bind("QuickMods/Brake", "BrakeWhenControlLost", false, "Enable or disable brake when control is lost");
         _brakePreLaunchAtLoad = config.Bind("QuickMods/Brake", "BrakePreLaunchAtLoad", false, "Enable or disable brake before launch");
         _brakeLandedRoverAtLoad = config.Bind("QuickMods/Brake", "BrakeLandedRoverAtLoad", false, "Enable or disable brake landed rover at load");
         _brakeLandedPlaneAtLoad = config.Bind("QuickMods/Brake", "BrakeLandedPlaneAtLoad", false, "Enable or disable brake landed plane at load");
         _brakeLandedVesselAtLoad = config.Bind("QuickMods/Brake", "BrakeLandedVesselAtLoad", false, "Enable or disable brake landed vessel at load");
-    }
-
-    public enum ToggleBrakeEnum
-    {
-        Modifier,
-        Toggle,
-        StockBrake
     }
 }
