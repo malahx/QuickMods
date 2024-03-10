7cfe7dc0aa63252c31835625a1b99e714a50690a
malahx <malahx@users.noreply.github.com>
2024-03-10 20:12:31 +0100

    fix quickpause name

--- src/QuickMods/configuration/impl/PauseConfiguration.cs
+++ src/QuickMods/configuration/impl/PauseConfiguration.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace QuickMods.configuration.impl;
 
-public class PauseConfiguration(ConfigFile config) : ConfigurationBase("QuickPauseControl")
+public class PauseConfiguration(ConfigFile config) : ConfigurationBase("QuickPause")
 {
     private ConfigEntry<bool> _enable;
     private ConfigEntry<KeyboardShortcut> _pauseKey;
@@ -18,7 +18,6 @@ public class PauseConfiguration(ConfigFile config) : ConfigurationBase("QuickPau
         base.Init();
 
         _enable = config.Bind("QuickMods/Pause", "Enable", false, "Enable or disable pause when escape");
-
         _pauseKey = config.Bind("QuickMods/Pause", "PauseKey",  new KeyboardShortcut(KeyCode.P), "Add a new key to toggle pause without using timewarp keys");
         _dontPauseWhenDecreaseWarp = config.Bind("QuickMods/StopWarp", "DontPauseWhenDecreaseWarp", false, "Enable or disable the default pause when decrease warp");
     }
