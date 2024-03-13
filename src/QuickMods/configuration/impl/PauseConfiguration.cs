a7dfacf5c539fd136a64acef0ed3930a1273f307
malahx <malahx@users.noreply.github.com>
2024-03-13 23:10:08 +0100

    added quickpause on editor

--- src/QuickMods/configuration/impl/PauseConfiguration.cs
+++ src/QuickMods/configuration/impl/PauseConfiguration.cs
@@ -5,11 +5,13 @@ namespace QuickMods.configuration.impl;
 
 public class PauseConfiguration(ConfigFile config) : ConfigurationBase("QuickPause")
 {
-    private ConfigEntry<bool> _enable;
+    private ConfigEntry<bool> _enableEscapePause;
+    private ConfigEntry<bool> _enableEditorPause;
     private ConfigEntry<KeyCode> _pauseKey;
     private ConfigEntry<bool> _dontPauseWhenDecreaseWarp;
 
-    public bool Enabled() => _enable.Value;
+    public bool EscapePauseEnabled() => _enableEscapePause.Value;
+    public bool EditorPauseEnabled() => _enableEditorPause.Value;
     public bool DontPauseWhenDecreaseWarp() => _dontPauseWhenDecreaseWarp.Value;
     public KeyCode PauseKey() => _pauseKey.Value;
 
@@ -17,8 +19,9 @@ public class PauseConfiguration(ConfigFile config) : ConfigurationBase("QuickPau
     {
         base.Init();
 
-        _enable = config.Bind("QuickMods/Pause", "Enable", false, "Enable or disable pause when escape");
-        _pauseKey = config.Bind("QuickMods/Pause", "PauseKey",  KeyCode.P, "Add a new key to toggle pause without using timewarp keys"); // new KeyboardShortcut(KeyCode.P) it seems to doesn't work with KSP!
+        _enableEscapePause = config.Bind("QuickMods/Pause", "EnableEscapePause", false, "Enable or disable pause when escape");
+        _enableEditorPause = config.Bind("QuickMods/Pause", "EnableEditorPause", false, "Enable or disable pause in the VAB/OAB/Editor");
+        _pauseKey = config.Bind("QuickMods/Pause", "PauseKey", KeyCode.P, "Add a new key to toggle pause without using timewarp keys"); // new KeyboardShortcut(KeyCode.P) it seems to doesn't work with KSP!
         _dontPauseWhenDecreaseWarp = config.Bind("QuickMods/StopWarp", "DontPauseWhenDecreaseWarp", false, "Enable or disable the default pause when decrease warp");
     }
 }
--- src/QuickMods/quick/impl/Pause.cs
+++ src/QuickMods/quick/impl/Pause.cs
@@ -13,8 +13,11 @@ public class Pause(PauseConfiguration config) : ModsBase(config)
     public override void Start()
     {
         base.Start();
+
         MessageCenter.Subscribe<EscapeMenuOpenedMessage>(OnEscapeMenuOpenedMessage);
         MessageCenter.Subscribe<EscapeMenuClosedMessage>(OnEscapeMenuClosedMessage);
+        MessageCenter.Subscribe<OABLoadedMessage>(OnOABLoadedMessage);
+        MessageCenter.Subscribe<OABUnloadedMessage>(OnOABUnloadedMessage);
 
         // Reset default TimeWarp key
         if (Game.InputManager.TryGetInputDefinition<GlobalInputDefinition>(out var definition))
@@ -29,8 +32,11 @@ public class Pause(PauseConfiguration config) : ModsBase(config)
     public override void OnDestroy()
     {
         base.OnDestroy();
+
         MessageCenter.Unsubscribe<EscapeMenuOpenedMessage>(OnEscapeMenuOpenedMessage);
         MessageCenter.Unsubscribe<EscapeMenuClosedMessage>(OnEscapeMenuClosedMessage);
+        MessageCenter.Unsubscribe<OABLoadedMessage>(OnOABLoadedMessage);
+        MessageCenter.Unsubscribe<OABUnloadedMessage>(OnOABUnloadedMessage);
     }
 
     public override void Update()
@@ -41,19 +47,33 @@ public class Pause(PauseConfiguration config) : ModsBase(config)
 
     private void OnEscapeMenuOpenedMessage(MessageCenterMessage msg)
     {
+        if (!config.EscapePauseEnabled()) return;
+
         _lastPauseValue = Game.ViewController.IsPaused;
         ForcePause(true);
     }
 
     private void OnEscapeMenuClosedMessage(MessageCenterMessage msg)
     {
+        if (!config.EscapePauseEnabled()) return;
+
         ForcePause(_lastPauseValue);
         _lastPauseValue = null;
     }
 
+    private void OnOABLoadedMessage(MessageCenterMessage msg)
+    {
+        if (config.EditorPauseEnabled() && !Game.ViewController.IsPaused) ForcePause(true);
+    }
+
+    private void OnOABUnloadedMessage(MessageCenterMessage msg)
+    {
+        if (config.EditorPauseEnabled() && Game.ViewController.IsPaused) ForcePause(false);
+    }
+
     private void ForcePause(bool? pause)
     {
-        if (!config.Enabled() || !pause.HasValue) return;
+        if (!pause.HasValue) return;
 
         var newPauseState = pause.GetValueOrDefault(false);
         Game.ViewController.SetPause(newPauseState);
@@ -69,7 +89,7 @@ public class Pause(PauseConfiguration config) : ModsBase(config)
         var num = Game.ViewController.TimeWarp.SetRateIndex(Game.ViewController.TimeWarp.CurrentRateIndex - 1, false) ? 1 : 0;
         if (num != 0)
             Game.ViewController.TimeWarp.CancelAutoWarp();
-        
+
         Logger.LogDebug("OnTimeWarpDecrease");
     }
 }
